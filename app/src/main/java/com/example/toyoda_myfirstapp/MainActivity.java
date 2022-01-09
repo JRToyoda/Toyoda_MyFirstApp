219dc39d1cadee12104aee9d1d787265885e8312
Jaen Toyoda <therubikators@gmail.com>
2022-01-09 19:18:13 +0800

    First Version

--- app/src/main/java/com/example/toyoda_myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/toyoda_myfirstapp/MainActivity.java
@@ -6,8 +6,11 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     MediaPlayer mediaPlayer;
     private Button button;
